fde60e48a0c411ddab421a33a2d7cbf6c27fd68c
Ryan Sanford <ryansanford@flywheel.io>
2016-07-21 15:57:00 -0500

    Don't trigger docker auto build if not a tag push

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,8 @@
 echo TRAVIS_TAG=$TRAVIS_TAG
+
+
+if [ -z ${TRAVIS_TAG} ] ; then
+  exit 0
+fi
 TRIGGER_PAYLOAD="{/"source_type/": \"Tag\", \"source_name\": \"$TRAVIS_TAG\"}"
 curl -H "Content-Type: application/json" --data "$TRIGGER_PAYLOAD" -X POST $DOCKERHUB_TRIGGER_URL
