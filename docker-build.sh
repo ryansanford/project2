30e1b039b231fd0f72c4d91eedd75529d783455b
Ryan Sanford <ryansanford@flywheel.io>
2016-07-21 16:06:19 -0500

    Resolve escaped quotes

--- docker-build.sh
+++ docker-build.sh
@@ -4,5 +4,5 @@ echo TRAVIS_TAG=$TRAVIS_TAG
 if [ -z ${TRAVIS_TAG} ] ; then
   exit 0
 fi
-TRIGGER_PAYLOAD="{/"source_type/": \"Tag\", \"source_name\": \"$TRAVIS_TAG\"}"
+TRIGGER_PAYLOAD="{\"source_type\": \"Tag\", \"source_name\": \"$TRAVIS_TAG\"}"
 curl -H "Content-Type: application/json" --data "$TRIGGER_PAYLOAD" -X POST $DOCKERHUB_TRIGGER_URL
