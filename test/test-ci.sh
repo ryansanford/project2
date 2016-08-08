75fe5dd4701fe184985089ab97541ad4622f1304
Ryan Sanford <ryansanford@flywheel.io>
2016-08-08 10:32:28 -0500

    Test orthanc build within travis

--- test/test-ci.sh
+++ test/test-ci.sh
@@ -7,4 +7,15 @@ set -e
 unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+
+# Orthanc
+ORTHANC_VERSION=Orthanc-1.1.0
+cd ~
+curl http://www.orthanc-server.com/downloads/get.php?path=/orthanc/$ORTHANC_VERSION.tar.gz | tar xz
+mkdir -p ~/OrthancBuild
+cd ~/OrthancBuild
+cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release ~/$ORTHANC_VERSION
+make
+
+
 echo "Tests completed without error."
