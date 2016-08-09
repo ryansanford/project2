35e1c371f4ade92edb22f4ed2a77b5c68d00609c
Ryan Sanford <ryansanford@flywheel.io>
2016-08-09 08:08:44 -0500

    Implement build cache for CI

--- test/test-ci.sh
+++ test/test-ci.sh
@@ -9,13 +9,21 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 
 # Orthanc
-ORTHANC_VERSION=Orthanc-1.1.0
-cd ~
-curl http://www.orthanc-server.com/downloads/get.php?path=/orthanc/$ORTHANC_VERSION.tar.gz | tar xz
-mkdir -p ~/OrthancBuild
-cd ~/OrthancBuild
-cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release ~/$ORTHANC_VERSION
-make
+if [ ! -f "~/${ORTHANC_BUILD}/Orthanc" ]; then
+  cd ~
+  curl http://www.orthanc-server.com/downloads/get.php?path=/orthanc/$ORTHANC_VERSION.tar.gz | tar xz
+  mkdir -p "~/$ORTHANC_BUILD"
+  cd "~/$ORTHANC_BUILD"
+  cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release ~/$ORTHANC_VERSION
+  make
+else
+  cd "~/$ORTHANC_BUILD"
+fi
+
+ls -al
+
+
+
 
 
 echo "Tests completed without error."
