cdd4bf3dc7e62664e06428703e906bc59e74ba7f
Ryan Sanford <ryansanford@flywheel.io>
2016-08-09 13:46:27 -0500

    Build Orthanc with relative locations

--- test/test-ci.sh
+++ test/test-ci.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-cd ~
 ls -al *
 
 # Orthanc
-if [ ! -f "~/${ORTHANC_BUILD}/Orthanc" ]; then
+(
+if [ ! -f "${ORTHANC_BUILD}/Orthanc" ]; then
   curl http://www.orthanc-server.com/downloads/get.php?path=/orthanc/$ORTHANC_VERSION.tar.gz | tar xz
-  mkdir -p "~/$ORTHANC_BUILD"
-  cd "~/$ORTHANC_BUILD"
-  cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release ~/$ORTHANC_VERSION
+  mkdir -p "$ORTHANC_BUILD"
+  cd "$ORTHANC_BUILD"
+  cmake -DSTATIC_BUILD=ON -DCMAKE_BUILD_TYPE=Release ../$ORTHANC_VERSION
   make
 else
-  cd "~/$ORTHANC_BUILD"
+  cd "$ORTHANC_BUILD"
 fi
 
 ls -al
+)
 
-
+ls -al *
 
 
 
